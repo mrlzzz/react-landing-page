355cc49cf0c1fe9923756796e67e6e549ee107b6
mrlzzz <morelzzzz@gmail.com>
2023-11-24 13:02:03 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import LandingPage from "./components/LandingPage";
+import "./App.css";
+
+function App() {
+    return (
+        <>
+            <LandingPage></LandingPage>
+        </>
+    );
+}
+
+export default App;
